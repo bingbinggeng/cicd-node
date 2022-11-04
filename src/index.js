653aadf64bfa6adaa8ece060b5238acc6be01773
tengyu <gengbingbing628@gmail.com>
2022-11-04 13:23:40 +0800

    增加websocket服务

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+/*
+ * @Author: bingbing.geng
+ * @Date: 2022-11-02 08:17:51
+ * @LastEditTime: 2022-11-04 08:17:52
+ * @FilePath: \cicd-node\src\index.js
+ */
 import Koa from 'koa'
 import Router from '@koa/router'
 import { initGlobalRoute } from './routes'
 import { handleResponse } from './middleware'
 import * as db from './mongoose'
 import KoaBody from 'koa-body'
+import createWebSocket from './websocket/app'
+
+createWebSocket()
 
 const app = new Koa();
 const router = new Router()
--- src/mongoose/index.js
+++ src/mongoose/index.js
@@ -1,14 +1,13 @@
 /*
  * @Author: bingbing.geng
  * @Date: 2022-11-02 08:17:51
- * @LastEditTime: 2022-11-02 08:44:33
+ * @LastEditTime: 2022-11-03 17:12:11
  * @FilePath: \cicd-node\src\mongoose\index.js
  */
 import mongoose from 'mongoose'
 import config from '../config'
 
 const db = mongoose.connection
-// console.log(config, 'config', db)
 
 export const connect = function () {
   mongoose.connect(config.db.uri)
